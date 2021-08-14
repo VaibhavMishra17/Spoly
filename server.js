660c0ddb11a6e6d580a8248c1372de558a31040e
Vaibhav Mishra <vmishra175@gmail.com>
2021-08-14 18:15:42 +0530

    added port and env file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000");
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
